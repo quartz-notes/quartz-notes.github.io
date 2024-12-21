468a53f203825cf90c3056f3997f557638c90ca4
unva <unva@unvas-MacBook-Air.local>
2024-12-21 19:46:35 +0700

    feat: sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,35 @@
+import { PlusIcon } from "lucide-react";
+import { cn } from "../lib/utils";
+import useStore from "../store";
+
 function Sidebar() {
+  const { notes, currentNote, setNote, addNote } = useStore();
+
   return (
-    <div className="w-64 border-r border-stone-900/10 text-white p-7"></div>
+    <div className="w-64 border-r border-stone-900/10 pt-10 pb-10 pl-5 pr-5">
+      <h1 className="text-3xl font-bold mb-4 p-2">Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li
+            key={note.id}
+            className={cn(
+              "cursor-pointer transition-colors hover:bg-stone-100 p-2 rounded",
+              note.id === currentNote &&
+                "bg-stone-300 text-stone-900 hover:bg-stone-300"
+            )}
+            onClick={() => setNote(note.id)}
+          >
+            Note {note.id}
+          </li>
+        ))}
+        <li
+          className="cursor-pointer text-center transition-colors p-2 rounded text-stone-900 hover:bg-stone-100"
+          onClick={() => addNote()}
+        >
+          <PlusIcon />
+        </li>
+      </ul>
+    </div>
   );
 }
 
