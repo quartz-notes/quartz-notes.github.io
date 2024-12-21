2c44851e68041a4329c33f8589297924885743a7
unva <unva@unvas-MacBook-Air.local>
2024-12-21 17:05:39 +0700

    feat: note editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+
 function App() {
-  return <div className="flex h-screen"></div>;
+  const editor = useCreateBlockNote();
+  return (
+    <div className="flex h-screen">
+      <div className="flex-1 p-10">
+        <BlockNoteView editor={editor} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
