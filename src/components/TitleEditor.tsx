4071d2cf1f0f4b44da8823d1bcb4e293d6335858
unva <unva@unvas-MacBook-Air.local>
2024-12-23 23:25:23 +0700

    fix: controlled input to uncontrolled

--- src/components/TitleEditor.tsx
+++ src/components/TitleEditor.tsx
@@ -8,7 +8,7 @@ function TitleEditor() {
   return (
     <input
       placeholder={`заметка ${currentNote}`}
-      value={notes.find((note) => note.id === currentNote)?.title}
+      value={notes.find((note) => note.id === currentNote)?.title || ""}
       onChange={(e) => updateNoteTitle(currentNote!, e.target.value)}
       className="pl-13 pr-13 mb-2 text-6xl font-bold outline-hidden placeholder:italic"
     />
