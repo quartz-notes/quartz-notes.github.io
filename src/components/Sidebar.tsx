4a1e483d0c5ab75978c8168e0fed602b8fca5666
unva <unva@unvas-MacBook-Air.local>
2024-12-23 13:12:07 +0700

    feat: note remove button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,13 @@
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, TrashIcon } from "lucide-react";
 import { cn } from "../lib/utils";
 import useNoteStore from "../stores/notes.store";
 
 function Sidebar() {
-  const { notes, currentNote, setCurrentNote, createNote } = useNoteStore();
+  const notes = useNoteStore((state) => state.notes);
+  const currentNote = useNoteStore((state) => state.currentNote);
+  const setCurrentNote = useNoteStore((state) => state.setCurrentNote);
+  const createNote = useNoteStore((state) => state.createNote);
+  const removeNote = useNoteStore((state) => state.removeNote);
 
   return (
     <div className="w-64 border-r border-stone-900/10 pt-10 pb-10 pl-5 pr-5">
@@ -13,13 +17,20 @@ function Sidebar() {
           <li
             key={note.id}
             className={cn(
-              "cursor-pointer transition-colors hover:bg-stone-100 p-2 rounded",
+              "flex justify-between align-middle cursor-pointer transition-colors hover:bg-stone-100 p-2 rounded",
               note.id === currentNote &&
-                "bg-stone-300 text-stone-900 hover:bg-stone-300"
+                "bg-stone-200 text-stone-900 hover:bg-stone-200"
             )}
             onClick={() => setCurrentNote(note.id)}
           >
             Note {note.id}
+            <TrashIcon
+              className="p-0.5 rounded transition-colors hover:bg-stone-300"
+              onClick={(e) => {
+                removeNote(note.id);
+                e.stopPropagation();
+              }}
+            />
           </li>
         ))}
         <li
