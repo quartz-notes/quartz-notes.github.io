d83056d91b34d8a4187d786bcb566786546de271
unva <unva@unvas-MacBook-Air.local>
2025-04-03 16:25:24 +0700

    chore: revert base url to root

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import "/src/app/index.css";
-import HomePage from "../pages/home-page";
-import { ThemeProvider } from "./providers/theme-provider";
-import LoginPage from "@/pages/login-page";
-import SignUpPage from "@/pages/signup-page";
+import LoginPage from '@/pages/login-page';
+import SignUpPage from '@/pages/signup-page';
+import { StrictMode } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import HomePage from '../pages/home-page';
+import { ThemeProvider } from './providers/theme-provider';
+import '/src/app/index.css';
 
 function App() {
-  return (
-    <StrictMode>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <BrowserRouter basename="/quartz-client">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </StrictMode>
-  );
+	return (
+		<StrictMode>
+			<ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+				<BrowserRouter basename={import.meta.env.BASE_URL}>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/signup' element={<SignUpPage />} />
+					</Routes>
+				</BrowserRouter>
+			</ThemeProvider>
+		</StrictMode>
+	);
 }
 
 export default App;
